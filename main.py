e521ce2527b868c5fb1eb8cc4df9513b5e374151
carollzambelli <caroll.zambelli@gmail.com>
2018-10-21 19:34:47 -0300

    finalizado

--- main.py
+++ main.py
@@ -1,57 +1,101 @@
-#import the html content and media class
+# import the html content and media class
 import fresh_tomatoes
 import media
 
 # This is where instances of the class Movie are defined.
-#The order of each input agrees with media class constructor
+# The order of each input agrees with media class constructor
 
-thor = media.Movie("Thor",
-                   "The powerful, but arrogant god Thor, is cast out of Asgard to live amongst humans in Midgard (Earth), where he soon becomes one of their finest defenders.",
-                   "images/thor.jpg",
-                   "https://www.youtube.com/watch?v=JOddp-nlNvQ")
+# Thor movie: movie title, storyline, poster, image and movie trailer
+thor = media.Movie(
+    "Thor",
+    "The powerful, but arrogant god Thor, is cast out of Asgard to live "
+    "amongst humans in Midgard (Earth), where he soon becomes one of their "
+    "finest defenders.",
+    "images/thor.jpg",
+    "https://www.youtube.com/watch?v=JOddp-nlNvQ"  # NOQA
+)
 
-iron_man = media.Movie("Iron Man",
-                   "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
-                   "images/iron_man.jpg",
-                   "https://www.youtube.com/watch?v=8hYlB38asDY")
+# Iron Man movie: movie title, storyline, poster, image and movie trailer
+iron_man = media.Movie(
+    "Iron Man",
+    "After being held captive in an Afghan cave, billionaire engineer Tony "
+    "Stark creates a unique weaponized suit of armor to fight evil.",
+    "images/iron_man.jpg",
+    "https://www.youtube.com/watch?v=8hYlB38asDY"  # NOQA
+)
 
-dr_strange = media.Movie("Dr. Strange",
-                   "While on a journey of physical and spiritual healing, a brilliant neurosurgeon is drawn into the world of the mystic arts.",
-                   "images/dr_strange.jpg",
-                   "https://www.youtube.com/watch?v=HSzx-zryEgM")
+# Dr Strange movie: movie title, storyline, poster, image and movie trailer
+dr_strange = media.Movie(
+    "Dr. Strange",
+    "While on a journey of physical and spiritual healing, a brilliant "
+    "neurosurgeon is drawn into the world of the mystic arts.",
+    "images/dr_strange.jpg",
+    "https://www.youtube.com/watch?v=HSzx-zryEgM"  # NOQA
+)
 
-pantera_negra = media.Movie("Black Panther",
-                   "T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country's past.",
-                   "images/black_panther.jpg",
-                   "https://www.youtube.com/watch?v=xjDjIWPwcPU")
+# Black Panther: movie title, storyline, poster, image and movie trailer
+pantera_negra = media.Movie(
+    "Black Panther",
+    "T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step "
+    "forward to lead his people into a new future and must confront a "
+    "challenger from his country's past.",
+    "images/black_panther.jpg",
+    "https://www.youtube.com/watch?v=xjDjIWPwcPU"  # NOQA
+)
 
-g_galaxy = media.Movie("Guardians of the Galaxy",
-                   "A group of intergalactic criminals are forced to work together to stop a fanatical warrior from taking control of the universe.",
-                   "images/g_galaxy.jpg",
-                   "https://www.youtube.com/watch?v=XdWyeT1VZrY")
+# Guardians of the Galaxy movie: movie title, storyline, poster, image and
+# movie trailer
+g_galaxy = media.Movie(
+    "Guardians of the Galaxy",
+    "A group of intergalactic criminals are forced to work together to stop a "
+    "fanatical warrior from taking control of the universe.",
+    "images/g_galaxy.jpg",
+    "https://www.youtube.com/watch?v=XdWyeT1VZrY"  # NOQA
+)
 
-spider_man = media.Movie("Spider Man",
-                   "Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.",
-                   "images/spider.jpg",
-                   "https://www.youtube.com/watch?v=39udgGPyYMg")
+# Spider Man movie: movie title, storyline, poster, image and movie trailer
+spider_man = media.Movie(
+    "Spider Man",
+    "Peter Parker balances his life as an ordinary high school student in "
+    "Queens with his superhero alter-ego Spider-Man, and finds himself on "
+    "the trail of a new menace prowling the skies of New York City.",
+    "images/spider.jpg",
+    "https://www.youtube.com/watch?v=39udgGPyYMg"  # NOQA
+)
 
-avengers = media.Movie("The Avengers",
-                   "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-                   "images/avengers.jpg",
-                   "https://www.youtube.com/watch?v=eOrNdBpGMv8")
+# The Avengers movie: movie title, storyline, poster, image and movie trailer
+avengers = media.Movie(
+    "The Avengers",
+    "Earth's mightiest heroes must come together and learn to fight as a team "
+    "if they are going to stop the mischievous Loki and his alien army from "
+    "enslaving humanity.",
+    "images/avengers.jpg",
+    "https://www.youtube.com/watch?v=eOrNdBpGMv8"  # NOQA
+)
 
-ant_man = media.Movie("Ant Man",
-                   "Armed with the astonishing ability to shrink in scale but increase in strength, master thief Scott Lang must embrace his inner-hero and help his mentor, Dr. Hank Pym, protect the secret behind his spectacular Ant-Man Suit from a new generation of towering threats.",
-                   "images/ant_man.jpg",
-                   "https://www.youtube.com/watch?v=pWdKf3MneyI")
+# Ant Man movie: movie title, storyline, poster, image and movie trailer
+ant_man = media.Movie(
+    "Ant Man",
+    "Armed with the astonishing ability to shrink in scale but increase in "
+    "strength, master thief Scott Lang must embrace his inner-hero and help "
+    "his mentor, Dr. Hank Pym, protect the secret behind his spectacular "
+    "Ant-Man Suit from a new generation of towering threats.",
+    "images/ant_man.jpg",
+    "https://www.youtube.com/watch?v=pWdKf3MneyI"  # NOQA
+)
 
-captain = media.Movie("Captain America",
-                   "Steve Rogers, a rejected military soldier transforms into Captain America after taking a dose of a 'Super-Soldier serum'",
-                   "images/cap_america.jpg",
-                   "https://www.youtube.com/watch?v=6y3oHJnfnjU")
+# Captain America movie: movie title, storyline, poster, image and movie
+# trailer
+captain = media.Movie(
+    "Captain America",
+    "Steve Rogers, a rejected military soldier transforms into Captain America"
+    " after taking a dose of a 'Super-Soldier serum'",
+    "images/cap_america.jpg",
+    "https://www.youtube.com/watch?v=6y3oHJnfnjU"  # NOQA
+)
 
-
-#we create a list for pass the movies into open_movies_page
-movies = [captain, iron_man, thor, dr_strange, avengers, pantera_negra, ant_man, spider_man, g_galaxy]
+# we create a list for pass the movies into open_movies_page
+movies = [captain, iron_man, thor, dr_strange, avengers, pantera_negra,
+          ant_man, spider_man, g_galaxy]
 
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,11 +1,26 @@
 import webbrowser
 
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        '''
+        This class storage the movie information (title, storyline, image and the url trailer)
+        :param movie_title: string
+        :param movie_storyline: string
+        :param poster_image: string
+        :param trailer_youtube: string
+        '''
+
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+
+        '''
+        This function is responsable for display the movie trailer
+        '''
+
         webbrowser.open(self.trailer_youtube_url)
